3cb57354a911b8fdcb366260fdf9c75f13b7016c
Theodor Tonum <theodor@tonum.no>
2018-08-12 20:10:54 +0200

    Use reservation state to set cleared/uncleared

--- src/ynabsync/import_worker.cr
+++ src/ynabsync/import_worker.cr
@@ -21,7 +21,7 @@ module Ynabsync
                   json.field "date", transaction.interestDate
                   json.field "amount", amount
                   json.field "memo", transaction.text
-                  json.field "cleared", transaction.accountingDate ? "cleared" : "uncleared"
+                  json.field "cleared", transaction.isReservation ? "uncleared" : "cleared"
                   json.field "approved", true
                   json.field "import_id", "YNAB:V3:#{amount}:#{transaction.interestDate.to_s("%F")}:1"
                 end
